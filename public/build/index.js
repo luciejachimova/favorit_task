ee9d38cc9e35fbb51d10eda7ac0c2990ae3834b9
luciejachimova <107799001+luciejachimova@users.noreply.github.com>
2024-03-08 21:36:36 +0100

    Added public folder

---
+++ public/build/index.js
@@ -0,0 +1,18 @@
+(self["webpackChunkfavorit_task"] = self["webpackChunkfavorit_task"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+alert("hello from webpack!");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
